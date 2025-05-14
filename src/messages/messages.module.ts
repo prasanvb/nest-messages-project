b63f09f03553ba090794e3c07057a6fc10d3570c
prasanvb <34255378+prasanvb@users.noreply.github.com>
2025-05-14 08:36:29 -0700

    Update messages.module.ts

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessageService } from './messages.service';
+import { MessageRepositories } from './messages.repositories';
 
 @Module({
   controllers: [MessagesController],
+  providers: [MessageService, MessageRepositories]
 })
 export class MessagesModule {}
